a0bb4f701ad0c71a18b08c87ce7f1746f730aee9
rayfiyo <rayfiyorex@gmail.com>
2024-02-16 17:46:06 +0900

    remove comment out

--- cmd/navigate/subCourse.go
+++ cmd/navigate/subCourse.go
@@ -34,18 +34,9 @@ func SubCourse(ctx context.Context, subCourse int) error {
 		units = v
 	}
 	fmt.Printf("units: %d\n", units)
-	/*
-		if err := chromedp.Run(ctx,
-			chromedp.Nodes(`//*[@class="label label-success"]`, &nodes, chromedp.AtLeast(1)),
-		); err != nil {
-			return xerrors.Errorf("Failed to get units count: %w", err)
-		}
-		units := len(nodes)
-	*/
 
 	// リンクがある行数（ノード数）を取得
 	nodes := 0
-	// for i := 1; i < 100; i++ {
 	if v, err := nodeCount(ctx,
 		`//*[@id="nan-contents"]/div[7]/div/table/tbody/tr`, chromedp.AtLeast(7),
 		// `//*[@id="nan-contents"]/div[7]/div/table/tbody/tr[`+fmt.Sprint(i)+`]/td`, chromedp.AtLeast(7),
@@ -54,53 +45,9 @@ func SubCourse(ctx context.Context, subCourse int) error {
 	} else {
 		nodes = v
 	}
-	// }
 	fmt.Printf("nodes: %d\n", nodes)
 
 	// ユニットの選択
-	/*
-		for i := 1; i < units+1; i++ {
-			if err := chromedp.Run(ctx,
-				chromedp.Nodes(`//*[@id="nan-contents"]/div[7]/div/table/tbody/tr[`+fmt.Sprint(i)+`]/td`, &nodes),
-			); err != nil {
-				return xerrors.Errorf("Failed to select units: %w", err)
-			}
-			for _, n := range nodes {
-				log.Println(n.Attributes)
-			}
-			fmt.Println("ok")
-		}
-	*/
-
-	/*
-		unitID := "JT01"
-		for i := 0; i < units; i++ {
-			if err := chromedp.Run(ctx,
-	*/
-	//chromedp.Text(`//*/td[@rowspan="2"][1]`, &unitID),
-	/*
-			); err != nil {
-				return xerrors.Errorf("Failed to select subcourse: %w", err)
-			}
-		}
-	*/
-
-	/*
-		// a11y.QueryAXTree().Do(ctx)
-		var nodeID cdp.NodeID
-		fmt.Println(a11y.QueryAXTree().WithRole("link").WithNodeID(nodeID))
-		fmt.Println(nodeID)
-
-
-			chromedp.Evaluate(`document.querySelector('`+selector+`').getAttribute('role')`, &role),
-			// var a11y *accessibility.GetPartialAXTreeParams
-			a11y := accessibility.GetPartialAXTree(accessibility.GetPartialAXTree().
-				WithNodeID(accessibility.GetAXNodeAndAncestors().NodeID))
-			if course == 1 && unitID[:1] == "U" {
-			}
-			_ = a11y
-			// func (p QueryAXTreeParams) WithRole(role string) *QueryAXTreeParams {
-	*/
 
 	fmt.Println("end")
 	time.Sleep(10 * time.Second)
