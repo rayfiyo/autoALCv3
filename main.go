c6cd066934c7133836c7f97455bcab19369eeaa6
rayfiyo <rayfiyorex@gmail.com>
2024-03-08 23:56:07 +0900

    Completed in one piece

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rayfiyo/autoALCv3/cmd"
 	"github.com/rayfiyo/autoALCv3/cmd/check"
 	"github.com/rayfiyo/autoALCv3/cmd/tasks"
+	"github.com/rayfiyo/autoALCv3/model"
 )
 
 func main() {
@@ -55,49 +56,66 @@ func main() {
 
 	// コースの選択
 	course := 0
-	subcourseNum := 0
+	subcourse := -1
 	fmt.Printf("コースの選択\nPowerWords Hybridコース -------------> 1\nTOEIC(R) L&R テスト 500点突破コース -> 2\n")
 	fmt.Scanln(&course)
 Loop:
 	for i := 0; ; i++ {
 		switch course {
 		case 1:
-			subcourseNum = 6
+			for subcourse < 1 || subcourse > 6 { // 1~6の範囲じゃないなら繰り返す
+				fmt.Print("サブコースの選択（1~6）: ")
+				fmt.Scanln(&subcourse)
+			}
 			break Loop
 		case 2:
-			subcourseNum = 5
+			for subcourse < 1 || subcourse > 5 { // 1~5の範囲じゃないなら繰り返す
+				fmt.Print("サブコースの選択（1~5）: ")
+				fmt.Scanln(&subcourse)
+			}
 			break Loop
 		default:
-			fmt.Printf("1 or 2: ")
+			fmt.Print("1 or 2: ")
 			fmt.Scanln(&course)
 		}
 	}
 
-	// 問題を解く処理
-	for subCrs := 1; subCrs < subcourseNum+1; subCrs++ {
-		// コース・サブコースの遷移
-		if err := cmd.Navigate(ctx, course, subCrs); err != nil {
-			log.Panic(err)
-		}
+	fmt.Print("CookieのSessionIDの入力: ")
+	sID := "hoge"
+	fmt.Scanln(&sID)
 
-		// 遷移の確認
-		if err := check.URL(ctx, "https://nanext.alcnanext.jp/anetn/Student/StUnitList"); err != nil {
-			log.Panic("コース・サブコースの選択に失敗\n" + fmt.Sprintln(err))
-		}
-
-		// リンクがある行数（ノード数）を取得
-		nodeNum, err := cmd.NodeCount(ctx, `//*[@id="nan-contents"]/div[7]/div/table/tbody/tr`, chromedp.AtLeast(7))
-		if err != nil {
-			log.Panic(err)
-		}
-
-		// ユニットの選択と処理
-		for i := 1; i < nodeNum; i++ {
-			if _, err := tasks.GetId(ctx, i); err != nil {
-				log.Panic(err)
-			}
-		}
+	// コース・サブコースの遷移
+	if err := cmd.Navigate(ctx, course, subcourse); err != nil {
+		log.Panic(err)
 	}
 
-	log.Println("All done.")
+	// 遷移の確認
+	if err := check.URL(ctx, "https://nanext.alcnanext.jp/anetn/Student/StUnitList"); err != nil {
+		log.Panic("コース・サブコースの選択に失敗\n" + fmt.Sprintln(err))
+	}
+
+	// リンクがある行数（ノード数）を取得
+	nodeNum, err := cmd.NodeCount(ctx, `//*[@id="nan-contents"]/div[7]/div/table/tbody/tr`, chromedp.AtLeast(7))
+	if err != nil {
+		log.Panic(err)
+	}
+
+	// ユニットの選択と処理
+	var id model.Id
+	for i := 1; i < nodeNum+1; i++ {
+		log.Println("- * - * - * - * -")
+		log.Printf("%d/%d開始\n\n", i, nodeNum)
+
+		if id, err = tasks.GetId(ctx, i); err != nil {
+			log.Panic(err)
+		}
+		if id.CId != "ex.TC1" {
+			tasks.Submit(sID, id)
+		}
+
+		log.Printf("%d/%d完了", i, nodeNum)
+		log.Printf("- * - * - * - * -\n\n")
+	}
+
+	log.Println("すべて完了.")
 }
